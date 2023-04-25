83224a5d157cc4b0c5ee223529805854ef507a3c
httpcyberdata <shieldedsafety@gmail.com>
2023-04-25 14:27:30 -0400

    chore: created Cart,Order,Product & User js files in Models folder + added README

---
+++ README.md
@@ -0,0 +1,21 @@
+# Node.js E-Commerce App REST API with MongoDB
+- Tutorial URL: https://www.youtube.com/watch?v=rMiRZ1iRC0A
+- Tutorial description: Node MongoDB eCommerce Rest API using Stripe payment method. Node.js shopping API tutorial using express, MongoDB, and JWT for beginners. 
+
+___________
+
+### Software versions
+
+### Problems in the project:
+1.
+2.
+
+### How to use
+
+### Photos of project:
+
+### Apps used:
+
+### Coding styles I used:
+
+### What I learned
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
+const userRoute = require('./routes/user');
 dotenv.config();
 
 
@@ -11,12 +11,11 @@ mongoose.connect(process.env.MONGO_URL)
     .catch((err) => {
         console.log(err);     
     });
+    app.use(express.json())
+    app.use('/api/users', userRoute);
 
-    app.get('/pr', () => {
-        console.log('test is successful')
-    })
 
-app.listen(process.env.PORT, () => {
+    app.listen(process.env.PORT, () => {
     console.log("Backend server is running")
 });
 
