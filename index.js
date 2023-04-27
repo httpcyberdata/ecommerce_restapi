31df38c08837c331b79739ae7373d49d4c44141b
httpcyberdata <shieldedsafety@gmail.com>
2023-04-27 16:35:36 -0400

    fix: api/auth/login route returns user object and not headers error

--- index.js
+++ index.js
@@ -5,18 +5,17 @@ const dotenv = require('dotenv');
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
 dotenv.config();
-
+app.use(express.json())
 
 mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log("DB Connection Successful"))
     .catch((err) => {
         console.log(err);     
     });
-    app.use(express.json())
     app.use('/api/auth', authRoute);
     app.use('/api/users', userRoute);
 
     app.listen(process.env.PORT, () => {
-    console.log("Backend server is running")
-});
+        console.log("Backend server is running")
+    });
 
--- routes/auth.js
+++ routes/auth.js
@@ -23,7 +23,13 @@ router.post('/login', async (req, res) => {
         const user = await User.findOne({
             username: req.body.username
         });
-        const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC)
+        !user && res.status(401).json("Wrong credentials!")
+        const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC);
+        const password = hashedPassword.toString(CryptoJS.enc.Utf8);
+        
+        password !== req.body.password && res.status(401).json("Wrong credentials");
+        res.status(200).json(user);
+
     } catch(err) {
          res.status(500).json(err);
     }
